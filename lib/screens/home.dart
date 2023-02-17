a1a426bd0f6afb24767bf873fffeb8d2e4851a37
山本　堅心 <s2032149nb@s.chibakoudai.jp>
2023-02-17 18:24:04 +0900

    initial state

---
+++ lib/screens/home.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class Home extends StatelessWidget {
+  final String title;
+  const Home({super.key, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: const Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+}
