46d4e3f3bb491351651f30a1cf423c038f7735fa
Shun <shunsuke.takasu.job@gmail.com>
2023-02-19 23:56:48 +0900

    Add: Category Items

---
+++ lib/data/categories.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import '../models/categories.dart';
+
+const categoryData = [
+  Categories(
+    category: "drink",
+    title: "飲み物",
+    color: Colors.redAccent,
+    iconData: Icons.local_drink,
+  ),
+  Categories(
+    category: "place",
+    title: "場所",
+    color: Colors.white,
+    iconData: Icons.place,
+  ),
+  Categories(
+    category: "tool",
+    title: "道具",
+    color: Colors.white,
+    iconData: Icons.construction,
+  ),
+  Categories(
+    category: "vehicle",
+    title: "乗り物",
+    color: Colors.white,
+    iconData: Icons.directions_car,
+  ),
+  Categories(
+    category: "emotion",
+    title: "気持ち",
+    color: Colors.white,
+    iconData: Icons.emoji_emotions,
+  ),
+  Categories(
+    category: "greeting",
+    title: "挨拶",
+    color: Colors.white,
+    iconData: Icons.emoji_people,
+  ),
+  Categories(
+    category: "else",
+    title: "その他",
+    color: Colors.white,
+    iconData: Icons.info_rounded,
+  ),
+];
