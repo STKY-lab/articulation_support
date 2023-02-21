bf164c099089b0f7fb86f890eb1a5e3611a9bd58
Shun <shunsuke.takasu.job@gmail.com>
2023-02-21 23:28:06 +0900

    Edit: Firebase

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,62 +1,44 @@
+import 'package:articulation_support/providers/riverpod.dart';
+import 'package:articulation_support/screens/category_item.dart';
 import 'package:flutter/material.dart';
-import 'package:google_sign_in/google_sign_in.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../data/categories.dart';
 
-import './test.dart';
-
-class Home extends StatelessWidget {
-  final String title;
-  const Home({super.key, required this.title});
+class Home extends ConsumerWidget {
+  static const routeName = '/home';
+  const Home({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    final googleLogin = GoogleSignIn();
-
-    Firebase.initializeApp();
-    Future<void> signIn() async {
-      // Google認証
-      GoogleSignInAccount? signinAccount = await googleLogin.signIn();
-      if (signinAccount == null) {
-        return;
-      }
-
-      // Firebaseへの認証情報を取得
-      GoogleSignInAuthentication auth = await signinAccount.authentication;
-      final OAuthCredential credential = GoogleAuthProvider.credential(
-        idToken: auth.idToken,
-        accessToken: auth.accessToken,
-      );
-
-      // 認証情報をFirebaseAuthenticationに登録
-      User? user =
-          (await FirebaseAuth.instance.signInWithCredential(credential)).user;
-      // ログイン成功
-      if (user != null) {
-        // ログイン成功と同時にTitleへ遷移
-        Navigator.of(context).pushReplacementNamed(Test.routeName);
-
-        // FireStoreにusersを追加
-        await FirebaseFirestore.instance.collection('users').doc(user.uid).set(
-          {
-            'username': user.displayName,
-            'email': user.email,
-          },
-        );
-      }
-    }
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          child: const Text('Login'),
-          onPressed: signIn,
+  Widget build(BuildContext context, WidgetRef ref) {
+    final deviceHeight =
+        MediaQuery.of(context).size.height - AppBar().preferredSize.height;
+    final deviceWidth = MediaQuery.of(context).size.width;
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        body: GridView(
+          padding: const EdgeInsets.all(8),
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10,
+          ),
+          children: categoryData
+              .map(
+                (categoryData) => CategoryItem(
+                  category: categoryData.category,
+                  title: categoryData.title,
+                  color: categoryData.color,
+                  iconData: categoryData.iconData,
+                ),
+              )
+              .toList(),
         ),
+        // backgroundColor: Theme.of(context).colorScheme.primary,
       ),
     );
   }
 }
+// Name
+// Text(FirebaseAuth.instance.currentUser!.displayName!),
